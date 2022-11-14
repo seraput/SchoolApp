d6b8261a5000f73ed3aaffe3606717493cfbf574
Septian <septian@merindo.co.id>
2022-11-14 17:02:05 +0700

    migrasi api dan database

--- app/src/main/java/com/example/schoolapp/helper/Server.java
+++ app/src/main/java/com/example/schoolapp/helper/Server.java
@@ -1,7 +1,7 @@
 package com.example.schoolapp.helper;
 
 public class Server {
-    public static final String URL_API = "https://mydbskripsi.000webhostapp.com/restfull/api-daring/"; //public api
-//    public static final String URL_PDF = "https://mydbskripsi.000webhostapp.com/hrd-pdf/"; //public pdf
+    public static final String URL_API_I = "https://mydbskripsi.000webhostapp.com/restfull/api-daring/"; //public api
+    public static final String URL_API = "https://mertrack.co.id/myrest/"; //public pdf
 //    public static final String URL_API = "http://maprojek.xyz/restfull/api-daring/";
 }
